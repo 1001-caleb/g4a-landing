0bf6b0e5f5ec21595dd7b1b5bc00b5c6462cd591
1001-caleb <pedroperez@ejemplo.com>
2022-06-13 15:54:10 -0500

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 export default function Navbar() {
-  const Button = styled.a`
-    color: red;
-  `
-
   return (
     <>
-      <Button>aloha</Button>
-   </>
-  )
+      <NabarContainer>
+        <h2>G4A</h2>
+        <div className="links">
+          <a href="">Conocenos</a>
+          <a href="">Aprende</a>
+          <a href="">Comunidad</a>
+        </div>
+      </NabarContainer>
+    </>
+  );
 }
+
+const NabarContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 4rem;
+
+  h2 {
+    font-size: 3rem;
+    color: #0A033C;
+  }
+
+  .links {
+    display: flex;
+    gap: 4rem;
+
+     a {
+        text-decoration: none;
+        font-size: 2rem;
+        color: #0A033C;
+      }
+  }
+
+`;
