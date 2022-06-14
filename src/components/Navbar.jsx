da4fa8cc93a37dbfbe8e903f1d5ef42c0eb6cd81
Caleb Parrra <mccalebyt@gmail.com>
2022-06-14 18:41:59 -0500

    bg nav animationr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         <div className="burguer">
           <BurguerButton clicked={clicked} handleClick={handleClick}/>
         </div>
-        
+        <BgDiv className={`initial ${clicked ? ' active' : ''}`}></BgDiv>
       </NabarContainer>
     </>
   );
@@ -84,3 +84,22 @@ const NabarContainer = styled.nav`
     text-align: center;
   }
 `;
+
+const BgDiv = styled.div`
+  background-color: #222;
+  position: absolute;
+  top: -1000px;
+  left: -1000px;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  transition: all .6s ease ;
+  
+  &.active{
+    border-radius: 0 0 80% 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
