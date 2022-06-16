06a215af31434543a7648ff5dab980034742ad63
Caleb Parrra <mccalebyt@gmail.com>
2022-06-16 17:54:27 -0500

    cards

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from "../src/components/Navbar"
 import Hero from '../src/components/Hero'
-
+import CardsContainer from '../src/components/CardsContainer'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar></Navbar>
     <Hero/>
+    <CardsContainer/>
   </React.StrictMode>
 )
